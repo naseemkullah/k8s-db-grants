02b8b6fddc7f9b41c520c55ca800f6a2b3ef28ee
Naseem <naseemkullah@gmail.com>
2019-05-30 09:49:33 -0400

    Use json logger by default for errors

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
+const process = require('process');
 const { logger } = require('./lib/logger');
 const mysqlGranter = require('./lib/mysqlGranter');
 const postgresqlGranter = require('./lib/postgresqlGranter');
 
-try {
-  const grantsFile = yaml.safeLoad(fs.readFileSync('grants.yml', 'utf8'));
-  const { mysql: mysqlGrants, postgresql: postgresqlGrants } = grantsFile;
+// use json logger for errors
+process
+  .on('unhandledRejection', (err) => {
+    logger.error(err);
+  })
+  .on('uncaughtException', (err) => {
+    logger.error(err);
+    process.exit(1);
+  });
 
-  if (!Array.isArray(mysqlGrants) || !mysqlGrants.length) {
-    logger.info('No mysql privileges to grant');
-  } else {
-    mysqlGranter(mysqlGrants);
-  }
 
-  if (!Array.isArray(postgresqlGrants) || !postgresqlGrants.length) {
-    logger.info('No postgresql privileges to grant');
-  } else {
-    postgresqlGranter(postgresqlGrants);
-  }
-} catch (error) {
-  logger.error(error);
+const grantsFile = yaml.safeLoad(fs.readFileSync('grants.yml', 'utf8'));
+const { mysql: mysqlGrants, postgresql: postgresqlGrants } = grantsFile;
+
+if (!Array.isArray(mysqlGrants) || !mysqlGrants.length) {
+  logger.info('No mysql privileges to grant');
+} else {
+  mysqlGranter(mysqlGrants);
+}
+
+if (!Array.isArray(postgresqlGrants) || !postgresqlGrants.length) {
+  logger.info('No postgresql privileges to grant');
+} else {
+  postgresqlGranter(postgresqlGrants);
 }
